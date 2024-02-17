0f0232e48f36313e2ebbdab6c71070a80f13596c
cj-mchatton <cjmchatton8@gmail.com>
2024-02-16 19:02:07 -0800

    Added simple routing (without React routing) and a Learn (instructional) page
    -App.jsx now has routing using a switch statement
    -Added the Learn (or instructional) page
    -Need to implement the instructional page
    -Need to add implementation with the navbar such that the active page is highlighted
    -Need to fix the placement of the footer, as it is always right underneath the components displayed on the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,33 @@ import Header from "./Global/header/Header";
 import Footer from "./Global/footer/Footer";
 import Home from "./Pages";
 
-
 function App() {
+    let component;
+
+    switch (window.location.pathname) {
+        case "/":
+            component = <Home />;
+            break;
+        case "/home":
+            component = <Home />;
+            break;
+        case "/about":
+            component = <h1>About</h1>;
+            break;
+        case "/instructional-page":
+            component = <h1>Instructional Page</h1>;
+            break;
+        default:
+            component = <h1>404 Not Found</h1>;
+            break;
+    }
+
     return (
         <>
             <header>
                 <Header />
             </header>
-            <div className="PageContent" >
-                
-                <Home />
-                
-            </div>
+            <div className="PageContent">{component}</div>
             <footer>
                 <Footer />
             </footer>
