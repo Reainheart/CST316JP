afe8adae094429e543991a9df96537e4f36d719f
Reainheart <noah.etch@gmail.com>
2024-02-22 19:56:42 -0800

    API Calling to DB

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const supabaseUserSession = require('./SupabaseClient').GetSupabaseUserSession;
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(express.json());
+
+app.get('/GetStructures', require('./SB/GetStructures'));
 
 app.listen(port, () => {
-    console.log('Example app listening on port ${port}');
+    console.log(`Example app listening on port ${port}`);
 });
