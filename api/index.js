491c86197cf0c8f1c7d3d4ff5371b7fc7ce4e30d
Alex S <alkapwnz@gmail.com>
2024-02-23 18:54:57 -0800

    cleaned up index.js to function, added getNode.js

--- api/index.js
+++ api/index.js
@@ -5,27 +5,11 @@ const port = 3000;
 
 app.use(express.json());
 
-
 app.get('/GetStructures', require('./SB/GetStructures'));
 
-app.get('/', async (req, res) => {
-    try {
-
-        const { data, error } = await supabaseUserSession()
-            .from('Objects')
-            .select('StructureName, StructureDescription');
-        
-        if (error) {
-            throw new Error(error.message);
-        }
-
-        res.json(data);
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+app.get('/GetNode', require('./SB/GetNode'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
