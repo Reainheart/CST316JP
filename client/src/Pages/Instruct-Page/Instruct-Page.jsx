7e031939822910f61ad1c4731b7e12353b91f2a7
cj-mchatton <cjmchatton8@gmail.com>
2024-03-07 14:06:10 -0800

    Added buttons to Instruct-Page
    -Need more implementation

--- client/src/Pages/Instruct-Page/Instruct-Page.jsx
+++ client/src/Pages/Instruct-Page/Instruct-Page.jsx
@@ -10,6 +10,43 @@ const InstructionalPage = () => {
             <div>
                 <h1>Learn</h1>
                 <p>Instructions here</p>
+
+                <div className="button-group">
+                    <Button size="large" variant="outlined">
+                        Node
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        Standard Linked List
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        Doubly Linked List
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        Circlular Linked List
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        Doubly Circular Linked List
+                    </Button>
+
+                    <Button size="large" variant="outlined">
+                        Binary Search Tree
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        Binary Tree
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        General Tree
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        Red Black Tree
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        Segment Tree
+                    </Button>
+                    <Button size="large" variant="outlined">
+                        Treap
+                    </Button>
+                </div>
             </div>
         </>
     );
