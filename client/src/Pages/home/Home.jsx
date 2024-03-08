a8a47b113ff870f1114cb5b372e0930f3153767f
cj-mchatton <cjmchatton8@gmail.com>
2024-03-08 14:47:56 -0800

    Learning-Tool-Menu
    -Added learning tool menu functionality
    -Memory View and Functions View still need content/implementation
    -Issue: Using the Code View changes the width of the Learning-Tool-Menu

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./home.css";
 
-import CanvasComponent from "./Components/Canvas/CanvasComponent"
+import CanvasComponent from "./Components/Canvas/CanvasComponent";
 import CodeView from "./Components/Learning-Tool-Menu/Code-View/Code-View";
+import LearningToolMenu from "./Components/Learning-Tool-Menu/Learning-Tool-Menu-View/Learning-Tool-Menu/Learning-Tool-Menu";
 
 const Home = () => {
     return (
         <>
             <div className="flex-container">
-                <CanvasComponent className="canvas"/>
-                <CodeView className="learning-tool-menu"/>
+                <CanvasComponent className="canvas" />
+                <LearningToolMenu className="learning-tool-menu" />
             </div>
         </>
     );
