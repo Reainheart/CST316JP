85ecaa81a556e89680af58e27ef6029e14adfebf
cj-mchatton <cjmchatton8@gmail.com>
2024-03-07 19:11:01 -0800

    Instruct-Page buttons
    -Added basic styling to buttons on the Instruct-Page
    -Lots of changes and implementation needed

--- client/src/Pages/Instruct-Page/Instruct-Page.jsx
+++ client/src/Pages/Instruct-Page/Instruct-Page.jsx
@@ -11,41 +11,42 @@ const InstructionalPage = () => {
                 <h1>Learn</h1>
                 <p>Instructions here</p>
 
-                <div className="button-group">
-                    <Button size="large" variant="outlined">
-                        Node
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        Standard Linked List
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        Doubly Linked List
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        Circlular Linked List
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        Doubly Circular Linked List
-                    </Button>
-
-                    <Button size="large" variant="outlined">
-                        Binary Search Tree
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        Binary Tree
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        General Tree
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        Red Black Tree
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        Segment Tree
-                    </Button>
-                    <Button size="large" variant="outlined">
-                        Treap
-                    </Button>
+                <div className="button-group-container">
+                    <div className="button-group">
+                        <Button size="large" variant="outline-dark">
+                            Node
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Standard Linked List
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Doubly Linked List
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Circlular Linked List
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Doubly Circular Linked List
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Binary Search Tree
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Binary Tree
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            General Tree
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Red Black Tree
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Segment Tree
+                        </Button>
+                        <Button size="large" variant="outline-dark">
+                            Treap
+                        </Button>
+                    </div>
                 </div>
             </div>
         </>
