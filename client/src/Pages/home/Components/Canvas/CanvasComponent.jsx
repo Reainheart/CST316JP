0dd3fcd3b59d994e35ee007af9fdbbcbb7cc5d21
cj-mchatton <cjmchatton8@gmail.com>
2024-03-21 10:47:49 -0700

    Node now displays on the canvas
    -Node is a React component
    -Upon clicking the canvas, a Node is displayed
    -Upon clicking a Node, the Node is removed
    -Can create multiple Nodes on the screen
    
    -Possible issue: Nodes can be placed on top of other Nodes if the user clicks on the canvas too close to an already existing Node

--- client/src/Pages/home/Components/Canvas/CanvasComponent.jsx
+++ client/src/Pages/home/Components/Canvas/CanvasComponent.jsx
@@ -1,35 +1,53 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-import { useRef } from "react";
+// Adjustments to CanvasComponent
+import React, { useState, useRef } from "react";
+import Node from "./Node/Node"; // Assuming you have a Node component
 import "./canvasComponent.css";
 
-const CanvasComponent = ({HomeWidth, HomeHeight}) => {
-    const canvasRef = useRef(null); // reference to the canvas element
-    const structureData = useRef(new Map())
+const CanvasComponent = ({ HomeWidth, HomeHeight }) => {
+    const canvasRef = useRef(null);
+    const [nodes, setNodes] = useState([]); // State to track nodes
 
-    // handle click event on the canvas (create a node or remove a node)
-    // show popup if the mouse is inside the node
-    // if node hasn't been created, create a node
-    // if node has been created, remove the node
     const handleClick = (event) => {
         const rect = canvasRef.current.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
+        const x = event.clientX - rect.left - 50; // Centering the node, assuming node's width/height is 100px
+        const y = event.clientY - rect.top - 50; // Adjust values as per your Node component's size
 
-        console.log('Canvas cords' +x+','+y)
+        // Adding a new node. Each node needs a unique key/id.
+        const newNode = { id: Math.random(), x, y, text: "Node" };
+        setNodes([...nodes, newNode]);
     };
 
+    // Function to handle node click: remove the node
+    const handleNodeClick = (id) => (event) => {
+        event.stopPropagation(); // Prevent the canvas click from firing
+        setNodes(nodes.filter((node) => node.id !== id));
+    };
 
-    // render the canvas and the popup (if the mouse is inside the node)
     return (
-        <>
+        <div
+            style={{
+                position: "relative",
+                width: HomeWidth,
+                height: HomeHeight,
+            }}
+        >
             <canvas
                 ref={canvasRef}
                 width={HomeWidth}
                 height={HomeHeight}
                 onClick={handleClick}
+                style={{ position: "absolute" }}
             />
-        </>
+            {nodes.map((node) => (
+                <Node
+                    key={node.id}
+                    x={node.x}
+                    y={node.y}
+                    text={node.text}
+                    onClick={handleNodeClick(node.id)}
+                />
+            ))}
+        </div>
     );
 };
 
