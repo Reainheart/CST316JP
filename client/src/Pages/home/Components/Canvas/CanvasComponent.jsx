1cfee1e9022b09af86b8634c2e578a8802869445
Milad Aleali <milad.moravej80@gmail.com>
2024-04-19 04:02:11 -0700

    nodes are now centered on the cursor, added border detection for top and bottom of canvas.

--- client/src/Pages/home/Components/Canvas/CanvasComponent.jsx
+++ client/src/Pages/home/Components/Canvas/CanvasComponent.jsx
@@ -9,7 +9,12 @@ const CanvasComponent = ({ HomeWidth, HomeHeight }) => {
     const handleClick = (event) => {
         const rect = canvasRef.current.getBoundingClientRect();
         const x = event.clientX - rect.left - 40; // Centering the node (node's width and height are 80px)
-        const y = event.clientY - rect.top;
+        const y = event.clientY - rect.top + 60;
+        
+        const threshold = 50;
+        if(y < threshold + 50 || y > rect.height - threshold + 27) {
+            return;
+        } 
 
         // Adds a new node. Each node has a unique key/id
         const newNode = { id: Math.random(), x, y, text: "Node" };
