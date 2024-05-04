740685a7448e220af5583269af022378d716b874
Alex S <alkapwnz@gmail.com>
2024-05-03 19:28:56 -0700

    added SB functions for Linked List Learn Page and Example Code.

---
+++ api/SB/GetLinkedListLearnPage.js
@@ -0,0 +1,18 @@
+module.exports = async (req, res) => {
+try {
+    const SupabaseClient = require('../SupabaseClient');
+    const { data, error } = await SupabaseClient.anonSupabaseClient
+        .from('LearnPages')
+        .select('title, information')
+        .eq('title', 'About Linked Lists');
+    
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    res.json(data);
+
+} catch (error) {
+    res.status(500).json({ error: error.message });
+}
+}
