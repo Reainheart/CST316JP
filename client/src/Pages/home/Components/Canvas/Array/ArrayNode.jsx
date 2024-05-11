e2d0ef866d819cc3af847131ff662db0d64e0a03
Noah <Noah.etch@gmail.com>
2024-05-10 20:54:18 -0700

    Array Node spawns new array node in array

--- client/src/Pages/home/Components/Canvas/Array/ArrayNode.jsx
+++ client/src/Pages/home/Components/Canvas/Array/ArrayNode.jsx
@@ -1,34 +1,24 @@
-import React from 'react';
+// import React from 'react';
+import PropTypes from 'prop-types';
+import './Array.css'
+const ArrayNode = ({ text, onClick }) => {
 
-const ArrayNode = () => {
-  const handleClickTop = () => {
-    console.log('Top rectangle clicked');
-    // Add your desired functionality here
-  }
-
-  const handleClickBottom = () => {
-    console.log('Bottom rectangle clicked');
-    // Add your desired functionality here
-  }
-
-  const handleClickLeft = () => {
-    console.log('Left rectangle clicked');
-    // Add your desired functionality here
-  }
-
-  const handleClickRight = () => {
-    console.log('Right rectangle clicked');
-    // Add your desired functionality here
-  }
-
-  return (
-    <div style={{ width: '300px', height: '300px', border: '1px solid black', position: 'relative' }}>
-      <div style={{ width: '100%', height: '25%', position: 'absolute', top: '0', left: '0', backgroundColor: 'red' }} onClick={handleClickTop}></div>
-      <div style={{ width: '100%', height: '25%', position: 'absolute', bottom: '0', left: '0', backgroundColor: 'blue' }} onClick={handleClickBottom}></div>
-      <div style={{ width: '25%', height: '100%', position: 'absolute', top: '0', left: '0', backgroundColor: 'green' }} onClick={handleClickLeft}></div>
-      <div style={{ width: '25%', height: '100%', position: 'absolute', top: '0', right: '0', backgroundColor: 'yellow' }} onClick={handleClickRight}></div>
-    </div>
-  );
+    return (
+        <>
+            <div
+                style={{
+                    width: '50px',
+                    height: '50px'
+                }}
+                className='arrayNode'
+                onClick={onClick}>
+                {text}
+            </div>
+        </>
+    )
 }
-
+ArrayNode.propTypes = {
+    text: PropTypes.string,
+    onClick: PropTypes.func
+};
 export default ArrayNode;
