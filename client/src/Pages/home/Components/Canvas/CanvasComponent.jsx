0bd6e63839c8bcebcc2d26e869b66cf90c4cae37
milad-aleali <milad.moravej80@gmail.com>
2024-05-31 21:17:23 -0700

    stack work in progress

--- client/src/Pages/home/Components/Canvas/CanvasComponent.jsx
+++ client/src/Pages/home/Components/Canvas/CanvasComponent.jsx
@@ -5,6 +5,7 @@ import Pointer from "./Pointer/Pointer";
 import Array from "./Array/Array";
 import LinkedList from "./LinkedList/LinkedList";
 import Tree from "./Tree/Tree";
+import Stack from "./Stack/Stack"
 import "./canvasComponent.css";
 
 //Radius is the node's radius in pixels
@@ -24,7 +25,7 @@ const CanvasComponent = ({ objectToDraw, drawnCanvasObjects, HomeWidth, HomeHeig
     const [arrays, setArrays] = useState([]); // State to track pointers
     const [linkedLists, setLinkedLists] = useState([]); // State to track pointers
     const [trees, setTrees] = useState([]); // State to track trees
-
+    const [stacks, setStacks] = useState([]); // State to track stacks
     // Map to track selected objects
     const selectedObjects = useRef(new Map()); 
 
@@ -198,6 +199,10 @@ const CanvasComponent = ({ objectToDraw, drawnCanvasObjects, HomeWidth, HomeHeig
                 return getNewLinkedList(x, y)
             case 'Tree':
                 return getNewTree(x, y);
+            case 'Stack':
+                drawnCanvasObjects.current.set(newObject.id, newObject);
+                setStacks([...stacks, newObject]);
+                return newObject.id;
         }
     };
     // This can also be passed to objects Like Array to draw their new items
@@ -303,6 +308,17 @@ const CanvasComponent = ({ objectToDraw, drawnCanvasObjects, HomeWidth, HomeHeig
                     name={tree.id}
                     nodes={tree.nodes}
                     pointers={tree.pointers}
+                />
+            ))}
+            {stacks.map((stack) => (
+                <Stack
+                    key={stack.id}
+                    name={stack.id}
+                    x={stack.x}
+                    y={stack.y}
+                    text={stack.text}
+                    onClick={handleObjectClick(stack.id)}
+                    isSelected={selectedObjects.current.has(stack.id)}
                 />
             ))}
         </>
---
+++ client/src/Pages/home/Components/Canvas/Stack/Stack.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import StackNode from './StackNode';
+import './Stack.css';
+
+const Stack = ({ x, y, text, onClick, isSelected }) => {
+    const [isActive, setIsActive] = useState(false);
+    const [stackData, setStackData] = useState([]);
+
+    useEffect(() => {
+        setIsActive(isSelected);
+    }, [isSelected]);
+
+    const handleStackNodeClick = (event) => {
+        event.stopPropagation();
+        setIsActive(!isActive);
+        if (onClick) {
+            onClick(event);
+        }
+    };
+
+    const pushToStack = () => {
+        let newNode = { text: `added ${stackData.length}` };
+        setStackData([...stackData, newNode]);
+    };
+
+    const popFromStack = () => {
+        setStackData(stackData.slice(0, -1));
+    };
+
+    return (
+        <div className={`stack ${isSelected ? 'selected-stack' : ''}`} style={{ left: x, top: y }}>
+            <h3>{text}</h3>
+            <div className="stack-container">
+                {isSelected && <button onClick={pushToStack}>Push</button>}
+                {stackData.map((node, index) => (
+                    <StackNode
+                        key={index}
+                        display_index={index}
+                        display_text={node.text}
+                        onClick={handleStackNodeClick}
+                    />
+                ))}
+                {isSelected && <button onClick={popFromStack}>Pop</button>}
+            </div>
+        </div>
+    );
+};
+
+
+Stack.propTypes = {
+    name: PropTypes.number.isRequired,
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    isSelected: PropTypes.bool
+};
+
+export default Stack;
