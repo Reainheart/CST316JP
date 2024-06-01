ed788c0fa6c11655c67cfd7e72d7bbe1e002e3f3
Noah <Noah.etch@gmail.com>
2024-06-01 13:52:42 -0700

    Array Selection Start, Queue drawn on canvas

--- client/src/Pages/home/Components/Canvas/Array/Array.jsx
+++ client/src/Pages/home/Components/Canvas/Array/Array.jsx
@@ -4,25 +4,35 @@ import PropTypes from 'prop-types'
 import "./Array.css";
 import { useState, useEffect } from "react";
 
-const Array = ({ x, y, text, onClick, isSelected }) => {
-    const [isActive, setIsActive] = useState(false);
+const Array = ({ x, y, text, selected, toggleSelection, getPointer, removeMe }) => {
     const [arrayData, setArrayData] = useState([]);
+    const [content, setContent] = useState(text);
+    const [inputIsHidden, setInputIsHidden] = useState(true);
 
-    //const nodeRef = useRef(null);
+    useEffect(() => {
+        setInputIsHidden(true); // Hide the input initially or when the node is deselected
+    }, [selected]);
 
-    useEffect((isSelected) => {
-        setIsActive(isSelected)
-    }, [isSelected]);
-
-    const handleArrayNodeClick = (event) => {
+    const handleArrayClick = (event) => {
         // Prevents onClick from bubbling to the parent if it's nested
         event.stopPropagation();
+        toggleSelection(name);
+    };
 
+    const handleChange = (e) => {
+        setContent(e.target.value);
+    };
 
-        setIsActive(!isActive);
-        if (onClick) {
-            onClick(event);
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault(); // Prevents the default action of adding a new line
+            setInputIsHidden(true);
         }
+    };
+
+    const triggerEdit = (e) => {
+        e.stopPropagation();
+        setInputIsHidden(false);
     };
 
     const addToEndOfArray = () => {
@@ -36,62 +46,52 @@ const Array = ({ x, y, text, onClick, isSelected }) => {
 
     return (
         <>
-            {isSelected ? (
-
-                <div className="selected-array" style={{ left: x, top: y }}>
-                    <h3>{text}</h3>
-
-                    <div className="array-container" >
-                        <ArrayNode
-                            key='start'
-                            display_index='start'
-                            display_text='+'
-                            onClick={addToStartOfArray}
-                        />
-                        {arrayData.map((node, index) => (
-                            <ArrayNode
-                                key={index}
-                                display_index={index}
-                                display_text={node.text}
-                                onClick={handleArrayNodeClick}
-                            />
-                        ))}
-                        <ArrayNode
-                            key='end'
-                            display_index='end'
-                            display_text='+'
-                            onClick={addToEndOfArray}
-                        />
-                    </div>
-                </div>
-            ) : (
-                <div className="array" style={{ left: x, top: y }}>
-                    <h3>{text}</h3>
-
-                    <div className="array-container" >
-                        <ArrayNode
-                            key='start'
-                            display_index='start'
-                            display_text='+'
-                            onClick={addToStartOfArray}
-                        />
-                        {arrayData.map((node, index) => (
-                            <ArrayNode
-                                key={index}
-                                display_index={index}
-                                display_text={node.text}
-                                onClick={handleArrayNodeClick}
-                            />
-                        ))}
-                        <ArrayNode
-                            key='end'
-                            display_index='end'
-                            display_text='+'
-                            onClick={addToEndOfArray}
-                        />
-                    </div>
+            {selected && (
+                <div className="array-options" style={{ left: x - 10.5, top: y - 35 }}>
+                    <button onClick={triggerEdit}>✎</button>
+                    <button onClick={removeMe}>␥</button>
+                    <button onClick={getPointer}>→</button>
                 </div>
             )}
+            <div
+                className={selected ? "selected-array" : "array"}
+                style={{ left: x, top: y, border: selected ? '2px solid blue' : '1px solid gray' }}
+            >
+                <h3>{content}</h3>
+
+                <div
+                    className="array-container"
+                    onClick={handleArrayClick}>
+                    <ArrayNode
+                        key='start'
+                        display_index='start'
+                        display_text='+'
+                        onClick={addToStartOfArray}
+                    />
+                    {arrayData.map((node, index) => (
+                        <ArrayNode
+                            key={index}
+                            display_index={index}
+                            display_text={node.text}
+                        />
+                    ))}
+                    <ArrayNode
+                        key='end'
+                        display_index='end'
+                        display_text='+'
+                        onClick={addToEndOfArray}
+                    />
+                </div>
+                <input
+                    style={{ left: x - 50, top: y + 100 }}
+                    hidden={inputIsHidden}
+                    className="changeContent"
+                    type="text"
+                    value={content}
+                    onInput={handleChange}
+                    onKeyDown={handleKeyDown}
+                />
+            </div>
         </>
     );
 };
@@ -100,7 +100,9 @@ Array.propTypes = {
     x: PropTypes.number.isRequired,
     y: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-    isSelected: PropTypes.bool
+    selected: PropTypes.bool,
+    toggleSelection: PropTypes.func,
+    getPointer: PropTypes.func,
+    removeMe: PropTypes.func,
 }
 export default Array;
